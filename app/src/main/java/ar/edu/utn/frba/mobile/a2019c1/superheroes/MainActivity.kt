3addf43f81cb1057767a6eab1545c6a91a8c33fd
mona <martin.m94@hotmail.com>
2019-05-13 17:18:55 -0300

    Implemento Fragments

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/MainActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/MainActivity.kt
@@ -5,40 +5,55 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.content.Intent
+import androidx.appcompat.app.ActionBar
 import androidx.core.app.NotificationCompat.getExtras
-
-
-
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView.OnNavigationItemSelectedListener
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+	lateinit var toolbar: ActionBar
+
+    private val onNavigationItemSelectedListener = OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+            R.id.navigation_fight -> {
+				toolbar.title="Pelea"
+				val fightFragment = FightFragment.newInstance()
+				openFragment(fightFragment)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+            R.id.navigation_envelope -> {
+				toolbar.title="Sobre"
+				val envelopeFragment = EnvelopeFragment.newInstance()
+				openFragment(envelopeFragment)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+            R.id.navigation_cards -> {
+				toolbar.title="Cartas"
+				val cardsFragment = CardsFragment.newInstance()
+				openFragment(cardsFragment)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
-
+	private fun openFragment(fragment: Fragment) {
+		val transaction = supportFragmentManager.beginTransaction()
+		transaction.replace(R.id.container, fragment)
+		transaction.addToBackStack(null)
+		transaction.commit()
+	}
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+
+		super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+		toolbar = supportActionBar!!
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
 
         val username = intent.extras?.getString("username")
         this.title = "Welcome, $username!"
