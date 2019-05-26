0b0524cd180f60333deebc1a1d87f008551a709d
Ian Cincotta <ian.cincotta@mercadolibre.com>
2019-05-26 12:45:34 -0300

    Show user available cards in grid view

---
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/adapters/CardsAdapter.kt
@@ -0,0 +1,46 @@
+package ar.edu.utn.frba.mobile.a2019c1.superheroes.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.R
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.domain.Card
+import com.squareup.picasso.Picasso
+
+
+class CardsAdapter(private val cards: List<Card>, private val context: Context) : BaseAdapter() {
+
+	override fun getItem(position: Int) = UnsupportedOperationException("method not implemented")
+
+	override fun getItemId(position: Int) = throw UnsupportedOperationException("method not implemented")
+
+	override fun getCount() = throw UnsupportedOperationException("method not implemented")
+
+	override fun getView(position: Int, _convertView: View?, parent: ViewGroup?): View {
+		val card = cards[position]
+		var convertView = _convertView
+		if (convertView == null) {
+			convertView = LayoutInflater
+				.from(context)
+				.inflate(ar.edu.utn.frba.mobile.a2019c1.superheroes.R.layout.linearlayout_card, null)
+		}
+		val imageView = convertView!!.findViewById<ImageView>(R.id.imageview_card)
+		val nameTextView = convertView.findViewById<TextView>(R.id.textview_card_name)
+		val powerTextView = convertView.findViewById<TextView>(R.id.textview_card_power)
+		Picasso.get()
+			.load(card.thumbnail)
+			.resize(200, 200)
+			.centerCrop()
+			.placeholder(R.drawable.ic_launcher_foreground)
+			.error(R.drawable.ic_launcher_foreground)
+			.into(imageView)
+		nameTextView.text = card.name
+		powerTextView.text = context.getString(R.string.text_card_power, card.power)
+		return convertView
+	}
+
+}
--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/cards/CardsFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/cards/CardsFragment.kt
@@ -2,28 +2,47 @@ package ar.edu.utn.frba.mobile.a2019c1.superheroes.ui.cards
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.GridView
+import androidx.fragment.app.Fragment
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.R
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.R.id.cards_grid_view
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.adapters.CardsAdapter
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.services.ApiService
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.services.SessionsService
 
 class CardsFragment : Fragment() {
 
-	override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_cards, container, false)
+	private val apiService by lazy { ApiService(context!!) }
+
+	private val sessionService by lazy { SessionsService(context!!) }
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+		inflater.inflate(R.layout.fragment_cards, container, false)
+
+	override fun onResume() {
+		super.onResume()
+		getUserAvailableCards()
+	}
+
+	private fun getUserAvailableCards() {
+		sessionService.getLoggedUser()?.let { loggedUser ->
+			apiService.getUserAvailableCards(loggedUser, { cards ->
+				val gridView = view!!.findViewById(cards_grid_view) as GridView
+				val cardsAdapter = CardsAdapter(cards, context!!)
+				gridView.adapter = cardsAdapter
+			}, { error ->
+				println("Failed to get user cards - error: $error")
+			})
+		} ?: println("Failed to get logged user")
+
 	}
 
 	companion object {
-		fun newInstance(): CardsFragment {
-			val fragment = CardsFragment()
-			return fragment
-		}
+		@JvmStatic
+		fun newInstance() = CardsFragment()
 	}
 
 }
