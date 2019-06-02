f0460b8467c7c2379c98ffc4a49300859cd8e8b9
Ian Cincotta <ian.cincotta@mercadolibre.com>
2019-06-02 12:47:18 -0300

    Implement table layout for user's team

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/cards/CardsFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/cards/CardsFragment.kt
@@ -4,12 +4,19 @@ package ar.edu.utn.frba.mobile.a2019c1.superheroes.ui.cards
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity.CENTER
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.GONE
 import android.view.View.INVISIBLE
 import android.view.ViewGroup
 import android.widget.GridView
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TableRow.LayoutParams
+import android.widget.TableRow.LayoutParams.MATCH_PARENT
+import android.widget.TableRow.LayoutParams.WRAP_CONTENT
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.R
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.adapters.CardsAdapter
@@ -17,6 +24,7 @@ import ar.edu.utn.frba.mobile.a2019c1.superheroes.services.ApiService
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.services.SessionsService
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.ui.registration.RegistrationActivity
 import kotlinx.android.synthetic.main.fragment_cards.*
+
 
 class CardsFragment : Fragment() {
 
@@ -29,6 +37,7 @@ class CardsFragment : Fragment() {
 	override fun onResume() {
 		super.onResume()
 		getUserAvailableCards()
+		getUserTeam()
 	}
 
 	private fun getUserAvailableCards() {
@@ -44,6 +53,21 @@ class CardsFragment : Fragment() {
 				spinner.visibility = INVISIBLE
 			})
 		} ?: handleUserNotLogged()
+	}
+
+	private fun getUserTeam() {
+		val table = view!!.findViewById<TableLayout>(R.id.table_user_team)
+		val row = TableRow(context)
+		val rowLayoutParams = LayoutParams(MATCH_PARENT, 0, 1f)
+		row.layoutParams = rowLayoutParams
+		for (i in 0..3) {
+			val tvSuperhero = TextView(context)
+			tvSuperhero.layoutParams = LayoutParams(0, WRAP_CONTENT, 0.25f)
+			tvSuperhero.gravity = CENTER
+			tvSuperhero.text = "superhero $i"
+			row.addView(tvSuperhero)
+		}
+		table.addView(row, 0)
 	}
 
 	private fun handleUserNotLogged() {
