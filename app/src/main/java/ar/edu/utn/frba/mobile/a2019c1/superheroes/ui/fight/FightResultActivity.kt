f62f83fbfb33953cb9c6d00f972acaea9b1be8dd
Gaston Crivelli <gastonghc@gmail.com>
2019-06-30 04:07:04 -0300

    fight

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/fight/FightResultActivity.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/fight/FightResultActivity.kt
@@ -1,34 +1,33 @@
 package ar.edu.utn.frba.mobile.a2019c1.superheroes.ui.fight
 
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.R
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.adapters.CardsAdapter
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.domain.Fight
+import ar.edu.utn.frba.mobile.a2019c1.superheroes.domain.Team
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.services.ApiService
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.services.SessionsService
+import kotlinx.android.synthetic.main.activity_fight_result.*
 
 class FightResultActivity : AppCompatActivity() {
 
 	private val sessionService by lazy { SessionsService(this) }
 	private val apiService by lazy { ApiService(this) }
+	private lateinit var cardsAdapterUser: CardsAdapter
+	private lateinit var cardsAdapterOpponent: CardsAdapter
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_fight_result)
-		val fight = intent.getSerializableExtra("fightResult") as Fight
-		val opponent = fight.opponent
-		val winner = fight.winner
-		val opponentTeamId = opponent.team_id
-		val opponentName = opponent.nickname
 
+		val fight = intent.getSerializableExtra("fightResult") as Fight
 		sessionService.getLoggedUserTeamId()?.let { id ->
-			apiService.getTeam(id, { team ->
-				Log.d("MY_TEAM", team.toString())
-				apiService.getTeam(opponentTeamId, { opponentTeam ->
-					// TODO show teams
-					Log.d("OPPONENT_TEAM", opponentTeam.toString())
+			apiService.getTeam(id, { myTeam ->
+				apiService.getTeam(fight.opponent.team_id, { opponentTeam ->
+					showTeams(fight, myTeam, opponentTeam)
 				}, { error ->
 					Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
 				})
@@ -36,6 +35,38 @@ class FightResultActivity : AppCompatActivity() {
 				Toast.makeText(this, error.toString(), Toast.LENGTH_LONG).show()
 			})
 		}
+	}
+
+	fun showTeams(fight: Fight, myTeam: Team, opponentTeam: Team) {
+
+		textview_fight_user.text = sessionService.getLoggedUser()?.nickname
+		textview_fight_opponent.text = fight.opponent.nickname
+
+		recycleview_fight_user.adapter = cardsAdapterUser
+		cardsAdapterUser.replaceItems(myTeam.superheroes)
+
+		recycleview_fight_opponent.adapter = cardsAdapterOpponent
+		cardsAdapterOpponent.replaceItems(opponentTeam.superheroes)
+
+		btn_fight_dofight.setOnClickListener {
+			fight(fight.winner)
+		}
+	}
+
+	fun fight(winner: String) {
+
+		btn_fight_dofight.visibility = View.INVISIBLE
+		textview_fight_info.visibility = View.VISIBLE
+		textview_fight_info.text = getString(R.string.title_fight_loser)
+
+		if (sessionService.getLoggedUser()?.nickname == winner) {
+			textview_fight_info.text = getString(R.string.title_fight_winner)
+		}
+
+		btn_fight_close.setOnClickListener {
+			finish()
+		}
+
 	}
 
 }
