f7bed87392130c8bdf7e9a072c29ee33d05ad699
Mario Llanos <mario.llanos@pm.me>
2019-06-30 15:59:21 -0300

    Adding shake event listener.

--- app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/fight/FightFragment.kt
+++ app/src/main/java/ar/edu/utn/frba/mobile/a2019c1/superheroes/ui/fight/FightFragment.kt
@@ -2,6 +2,8 @@ package ar.edu.utn.frba.mobile.a2019c1.superheroes.ui.fight
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +11,11 @@ import androidx.fragment.app.Fragment
 import ar.edu.utn.frba.mobile.a2019c1.superheroes.R
 import kotlinx.android.synthetic.main.fragment_fight.view.*
 
-class FightFragment : Fragment() {
+class FightFragment : Fragment(), ShakeEventManager.ShakeListener {
+
+	private val shakeEventManager = ShakeEventManager()
+	private val handler = Handler()
+	private var shakeEnabled = false
 
 	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 		val view = inflater.inflate(R.layout.fragment_fight, container, false)
@@ -17,14 +23,34 @@ class FightFragment : Fragment() {
 		return view
 	}
 
+	override fun onResume() {
+		super.onResume()
+		shakeEventManager.setListener(this)
+		shakeEventManager.init(context!!)
+		handler.postDelayed({
+			shakeEnabled = true
+		}, 500)
+	}
+
+	override fun onStop() {
+		super.onStop()
+		shakeEventManager.setListener(null)
+	}
+
 	private fun searchFight() {
 		val intent = Intent(context!!, FightSearchActivity::class.java)
 		startActivity(intent)
+	}
+
+	override fun onShake() {
+		if (shakeEnabled) {
+			shakeEnabled = false
+			searchFight()
+		}
 	}
 
 	companion object {
 		@JvmStatic
 		fun newInstance() = FightFragment()
 	}
-
 }
